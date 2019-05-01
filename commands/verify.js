5eddb4974eb7f9355888d48aa6330f81d0d0af89
coldniight <49598352+coldniight@users.noreply.github.com>
2019-05-01 18:51:06 -0500

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -12,8 +12,8 @@ module.exports.run = async (bot, message, args) => {
     .setTimestamp();
 
     if(!message.member.roles.has(unverifiedrole.id)) return message.channel.send(alreadyembed);
-    message.author.addRole(verifiedrole.id);
-    message.author.removeRole(unverifiedrole.id);
+    await(message.member.addRole(verifiedrole.id));
+    await(message.member.removeRole(unverifiedrole.id));
 }
 
 module.exports.help = {
